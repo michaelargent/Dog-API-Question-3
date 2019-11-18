0c184107e6edb806c6d5a1aa9f17029c551843e8
michaelargent <53536596+michaelargent@users.noreply.github.com>
2019-11-18 09:45:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,28 +2,28 @@
 
 function getDogImage(breedOfDog) {
   console.log(breedOfDog)
-    fetch(`https://dog.ceo/api/breed/${breedOfDog}/images/random`)
-      .then(response => response.json())
-      .then(responseJson => 
-        displayResults(responseJson))
+  fetch(`https://dog.ceo/api/breed/${breedOfDog}/images/random`)
+    .then(response => response.json())
+    .then(responseJson => 
+          displayResults(responseJson))
       .catch(error => {
         console.log(error)
-      });
-  };
+  });
+};
 
-    function displayResults(responseJson) {
-        console.log(responseJson);
-        $(".results").html("");
-        $(".results").append(`<img src="${responseJson.message}" class="results">`);
-        //display the results section
-        $(".results").removeClass("hidden");
-      };
+function displayResults(responseJson) {
+  console.log(responseJson);
+  $(".results").html("");
+  $(".results").append(`<img src="${responseJson.message}" class="results">`);
+   //display the results section
+  $(".results").removeClass("hidden");
+};
 
 function watchSubmit() {
   $('form').submit(event => {
     event.preventDefault();
     let breedOfDog = $(event.target).find('#breedOfDog').val()
-    getDogImage(breedOfDog);
+ getDogImage(breedOfDog);
   });
 };
 
